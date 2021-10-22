3a5d904fe29ce29d292c6271db7e17a25833ef54
TomoyasuTaka <piano.cembalo.taka.2nd@gmail.com>
2021-10-22 23:54:16 +0900

    Change DirName

---
+++ Paiza/New Question/ListSort.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class ListSort
+{
+
+    public void Start()
+    {
+
+        List<string> takake = new List<string>();
+
+        takake.Add("Asumi");
+        takake.Add("Marina");
+        takake.Add("haruna");
+        takake.Add("tobikun");
+
+        takake[1] += "abc";
+
+        takake.Sort();
+
+        foreach (string a in takake)
+        {
+            Console.WriteLine(a);
+        }
+    }
+}
